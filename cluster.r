8d50672694cd975b6b2ff0bbb9879a99f197c0d8
laurentsuch <113410894+laurentsuch@users.noreply.github.com>
2023-07-17 10:40:27 -0700

    Update cluster.r

--- cluster.r
+++ cluster.r
@@ -16,6 +16,28 @@ library(nnet)
 library(caret)
 
 
+# FOLLoW THIS METHOD:
+# switch column "Approx how many hours of.." to column Z
+df <- df %>% mutate_if(is.character, as.factor)
+df[1] <- lapply(df[1], ordered, levels=c("One of my favorite NHL teams", "Second favorite NHL team", "Favorite NHL Team"))
+df[3] <- lapply(df[3], ordered, levels=c("18 to 24", "25 to 34", "35 to 44", "45 to 54", "55 to 64", "65 to 74", "75 or older"))
+# column 4 = change none to 0 and more than 5 to 6
+# convert 6-10 to 0/1
+df[11] <- lapply(df[11], ordered, levels=c("I started following during the 2023 Stanley Cup Playoffs", "I only recently started to follow the NHL (fewer than 6 years ago)",
+                                           "I started following when the Golden Knights came to Vegas", "I started following many years ago (more than 6 years ago)", "I have been a fan my whole life"))
+df[12] <- lapply(df[12], ordered, levels=c("I do not follow the NHL, just my favorite team", "I follow the NHL somewhat, but I couldn't name more than some teams and players", "I follow the NHL closely, and could 
+                                           talk about teams and players having a stong season", "I follow the NHL very closely, and could recount the standings as well as major events happening around the league"))
+df[13:25] <- lapply(df[13:25], ordered, levels=c("Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"))
+df[26] <- lapply(df[26], ordered, levels=c("<5 hours", "6-10 hours", "11-15 hours", "16-20 hours", ">20 hours"))
+df[27] <- lapply(df[27], ordered, levels=c("I did not follow them at all", "I casually followed them as part of the local news", "I actively followed them and can discuss team news in conversation", 
+                                           "I followed them closely, they are my favorite team", "I followed them very closely, I am emotionally invested in them"))
+df[28:38] <- lapply(df[28:38], ordered, levels=c("Not important at all", "Not important", "Neutral", "Important", "Extremely important"))
+df[41] <- lapply(df[41], ordered, levels=c("Fair Weather - My support for the team varies greatly depending on team performance.", "Somewhat Fair Weather - My support for the team varies somewhat depending on team performance.", 
+                                           "Neutral", "Somewhat Die-hard - My support for the team will remain strong, win or lose.", "Die-hard - My support for the team will remain very strong, win or lose."))
+df[42] <- lapply(df[42], ordered, levels=c("I do not live in the Las Vegas area", "Less than 2 years", "2-5 years", "5-10 years", ">10 years", "Born and raised"))
+
+
+
 # CLEANING DATA
 df <- df %>% mutate_if(is.character, as.factor) # converts all characters to factors
 ncol(df) # finds number of columns 
